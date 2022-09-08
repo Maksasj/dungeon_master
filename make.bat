9524384dc5591f725512e565c2ba6be966b4e7ee
maksasj <radioboos@gmail.com>
2022-09-08 20:25:55 +0300

    updated make file

--- make.bat
+++ make.bat
@@ -1,2 +1,8 @@
-gcc -o dist/main.elf src/main.c
-objcopy -O binary dist/main.elf dist/game.gba
+
+@ECHO OFF
+
+gcc -c -O3 src\main.c -o dist\main.o
+gcc -o dist\main.elf dist\main.o -lm
+
+if "%errorlevel%"=="1" pause
+if "%errorlevel%"=="0" objcopy -O binary dist\main.elf dist\main.gba
