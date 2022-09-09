acbd52e2beca1bf894855817d1f0516577c294c2
maksasj <radioboos@gmail.com>
2022-09-09 21:20:33 +0300

    Basic game engine structure implementation

---
+++ src/game_logic.h
@@ -0,0 +1,8 @@
+#ifndef GAME_LOGIC_H
+#define GAME_LOGIC_H
+
+int game_logic() {
+    return 0;
+}
+
+#endif
---
+++ src/graphics_engine.h
@@ -0,0 +1,8 @@
+#ifndef GRAPHICS_ENGINE_H
+#define GRAPHICS_ENGINE_H
+
+int graphics_engine() {
+    return 0;
+}
+
+#endif
