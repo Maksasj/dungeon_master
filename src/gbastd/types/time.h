10ca8d4b4856eb063cdaae7c5cb421d1b58ac460
Soskar1 <89640069+Soskar1@users.noreply.github.com>
2022-09-19 18:20:04 +0300

    ticks added

--- src/gbastd/types/time.h
+++ src/gbastd/types/time.h
@@ -2,11 +2,40 @@
 #define TIME_H
 
 #include "u.h"
+#include "../../defines.h"
 
-typedef struct {
+typedef struct
+{
     u8 seconds;
     u8 minutes;
     u8 hours;
-} time_u8;
+} time;
+
+u64 tick = 0;
+
+void addTick()
+{
+    tick += UPDATES_PER_SECOND;
+}
+
+time getTime()
+{
+    time time;
+    time.seconds = tick / UPDATES_PER_SECOND;
+
+    if (time.seconds > 59)
+    {
+        time.minutes = time.seconds / 60;
+        time.seconds -= time.minutes * 60;
+    }
+
+    if (time.minutes > 59)
+    {
+        time.hours = time.minutes / 60;
+        time.minutes -= time.hours * 60;
+    }
+
+    return time;
+}
 
 #endif
