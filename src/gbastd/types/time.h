71e312137b30730d2ce23e8b860cba5804edbf99
Soskar1 <89640069+Soskar1@users.noreply.github.com>
2022-09-20 09:29:20 +0300

    Code refactoring

--- src/gbastd/types/time.h
+++ src/gbastd/types/time.h
@@ -4,35 +4,30 @@
 #include "u.h"
 #include "../../defines.h"
 
-typedef struct
-{
+typedef struct {
     u8 seconds;
     u8 minutes;
     u8 hours;
+
+    u64 tick;
 } time;
 
-u64 tick = 0;
-
-void addTick()
-{
-    tick += UPDATES_PER_SECOND;
+void addTick(time* time) {
+    if (time->tick + 1 < U64_LIMIT)
+        time->tick++;
 }
 
-time getTime()
-{
-    time time;
-    time.seconds = tick / UPDATES_PER_SECOND;
+time* getTime(time* time) {
+    time->seconds = time->tick / UPDATES_PER_SECOND;
 
-    if (time.seconds > 59)
-    {
-        time.minutes = time.seconds / 60;
-        time.seconds -= time.minutes * 60;
+    if (time->seconds > 59) {
+        time->minutes = time->seconds / 60;
+        time->seconds -= time->minutes * 60;
     }
 
-    if (time.minutes > 59)
-    {
-        time.hours = time.minutes / 60;
-        time.minutes -= time.hours * 60;
+    if (time->minutes > 59) {
+        time->hours = time->minutes / 60;
+        time->minutes -= time->hours * 60;
     }
 
     return time;
--- src/gbastd/types/u.h
+++ src/gbastd/types/u.h
@@ -1,6 +1,11 @@
 #ifndef U_H
 #define U_H
 
+#define U8_LIMIT 255
+#define U16_LIMIT 65535
+#define U32_LIMIT 4294967295
+#define U64_LIMIT 18446744073709551615u
+
 typedef unsigned char u8;
 typedef unsigned short u16;
 typedef unsigned long  u32;
