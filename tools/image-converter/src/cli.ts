06984b94fcb5d7a0b359d80d6cc1d9d315e47c5c
Aleksandras Sukelovic <alex.shukel@gmail.com>
2022-09-22 11:12:46 +0300

    Added directory cleaning before generating images

--- tools/image-converter/src/cli.ts
+++ tools/image-converter/src/cli.ts
@@ -9,43 +9,53 @@ import { rgbToHex } from "./rgbToHex";
 
 const { inputPath, outputPath } = processArguments();
 
-const inputDir = await fs.promises.readdir(inputPath);
+const generateImages = async () => {
+    const inputDir = await fs.promises.readdir(inputPath);
 
-const inputFiles = inputDir.map((filePath) => path.join(process.cwd(), inputPath, filePath));
+    const inputFiles = inputDir.map((filePath) => path.join(process.cwd(), inputPath, filePath));
 
-for (const filePath of inputFiles) {
-    const fileName = path.parse(filePath).name;
+    for (const filePath of inputFiles) {
+        const fileName = path.parse(filePath).name;
 
-    fs.createReadStream(filePath)
-        .pipe(
-            new PNG({
-                filterType: 4,
-            })
-        )
-        .on("parsed", function () {
-            const length = this.width * this.height;
-            let output = `const unsigned short ${fileName}[${length}] = {\n`;
+        fs.createReadStream(filePath)
+            .pipe(
+                new PNG({
+                    filterType: 4,
+                })
+            )
+            .on("parsed", function () {
+                const length = this.width * this.height;
+                let output = `const unsigned short ${fileName}[${length}] = {\n`;
 
-            for (let y = 0; y < this.height; y++) {
-                output += "\t";
+                for (let y = 0; y < this.height; y++) {
+                    output += "\t";
 
-                for (let x = 0; x < this.width; x++) {
-                    const index = (this.width * y + x) << 2;
+                    for (let x = 0; x < this.width; x++) {
+                        const index = (this.width * y + x) << 2;
 
-                    const r = this.data[index],
-                        g = this.data[index + 1],
-                        b = this.data[index + 2];
+                        const r = this.data[index],
+                            g = this.data[index + 1],
+                            b = this.data[index + 2];
 
-                    const hex = rgbToHex(r, g, b);
+                        const hex = rgbToHex(r, g, b);
 
-                    output += `0x${hex.toString(16)},`;
+                        output += `0x${hex.toString(16)},`;
+                    }
+
+                    output += "\n";
                 }
 
-                output += "\n";
-            }
+                output += "};\n";
 
-            output += "};\n";
+                safeWriteFileSync(path.join(outputPath, `${fileName}.h`), output);
+            });
+    }
+};
 
-            safeWriteFileSync(path.join(outputPath, `${fileName}.h`), output);
-        });
-}
+fs.rm(outputPath, { recursive: true, force: true }, (err) => {
+    if (err) {
+        console.error(err);
+    } else {
+        generateImages();
+    }
+});
