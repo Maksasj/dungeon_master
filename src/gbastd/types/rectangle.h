325c1d3d823b530fb4a9e2e773701c965ad47853
Soskar1 <89640069+Soskar1@users.noreply.github.com>
2022-09-24 10:04:16 +0300

    Rectangle structure

---
+++ src/gbastd/types/rectangle.h
@@ -0,0 +1,13 @@
+#ifndef RECTANGLE_H
+#define RECTANGLE_H
+
+#include "u.h"
+#include "vector.h"
+
+typedef struct {
+    ivec2 startPosition;
+    int height;
+    int width;
+} Rectangle;
+
+#endif
