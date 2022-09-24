616937bb8d330ee96d3d657c42402df2253d350a
Soskar1 <89640069+Soskar1@users.noreply.github.com>
2022-09-24 10:53:17 +0300

    movingObjectsCollision method

--- src/gbastd/types/rectangle.h
+++ src/gbastd/types/rectangle.h
@@ -21,4 +21,14 @@ int checkCollision(Rectangle* rect1, Rectangle* rect2) {
     return 0;
 }
 
+int movingObjectsCollision(Rectangle rect1, Rectangle rect2, ivec2 rect1MovementDirection, ivec2 rect2MovementDirection) {
+    Rectangle tempRect1 = rect1;
+    Rectangle tempRect2 = rect2;
+
+    tempRect1.lowerLeftPoint = vec2I(rect1.lowerLeftPoint.x + rect1MovementDirection.x, rect1.lowerLeftPoint.y + rect1MovementDirection.y);
+    tempRect2.lowerLeftPoint = vec2I(rect2.lowerLeftPoint.x + rect2MovementDirection.x, rect1.lowerLeftPoint.y + rect2MovementDirection.y);
+
+    return checkCollision(&tempRect1, &tempRect2);
+}
+
 #endif
