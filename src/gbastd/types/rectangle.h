b928cab03917852eda4a823ccdbb42934cbf209c
Soskar1 <89640069+Soskar1@users.noreply.github.com>
2022-09-24 10:31:28 +0300

    checkCollision (rect vs rect)

--- src/gbastd/types/rectangle.h
+++ src/gbastd/types/rectangle.h
@@ -5,9 +5,20 @@
 #include "vector.h"
 
 typedef struct {
-    ivec2 startPosition;
+    ivec2 lowerLeftPoint;
     int height;
     int width;
 } Rectangle;
+
+int checkCollision(Rectangle* rect1, Rectangle* rect2) {
+    if (rect1->lowerLeftPoint.x <= rect2->lowerLeftPoint.x + rect2->width &&
+    rect1->lowerLeftPoint.x + rect1->width >= rect2->lowerLeftPoint.x &&
+     rect1->lowerLeftPoint.y <= rect2->lowerLeftPoint.y + rect2->height &&
+      rect1->lowerLeftPoint.y + rect1->height >= rect2->lowerLeftPoint.y) {
+        return 1;
+      }
+
+    return 0;
+}
 
 #endif
