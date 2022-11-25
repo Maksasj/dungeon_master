861e58516f6704b2f0cdcab1402e482260bc5ccf
Soskar1 <89640069+Soskar1@users.noreply.github.com>
2022-11-25 15:45:03 +0200

    naming convention

--- src/include/logger.h
+++ src/include/logger.h
@@ -6,16 +6,16 @@
 
 #include "types.h"
 
-typedef enum LOG_LEVEL {
+typedef enum LogLevel {
     LOG_FATAL                   = 0x100,
     LOG_ERR                     = 0x101,
     LOG_WARN                    = 0x102,
     LOG_INFO                    = 0x103
-} LOG_LEVEL;
+} LogLevel;
 
 #define _REG_LOG_ENABLE_          *(vu16*) 0x4FFF780
-#define _REG_LOG_LEVEL_           *(vu16*) 0x4FFF700
+#define _REG_LogLevel_           *(vu16*) 0x4FFF700
 
-inline void log(LOG_LEVEL lvl, const char* fmt, ...);
+inline void log(LogLevel _lvl, const char* _fmt, ...);
 
 #endif
--- src/logger.c
+++ src/logger.c
@@ -1,14 +1,14 @@
 #include "include/logger.h"
 
-inline void log(LOG_LEVEL lvl, const char* fmt, ...) {
+inline void log(LogLevel _lvl, const char* _fmt, ...) {
     _REG_LOG_ENABLE_ = 0xC0DE;
-    _REG_LOG_LEVEL_ = lvl;
+    _REG_LogLevel_ = _lvl;
 
     va_list args;
-    va_start(args, fmt);
+    va_start(args, _fmt);
 
     char* const log = (char*) 0x4FFF600;
-    vsnprintf(log, 0x100, fmt, args);
+    vsnprintf(log, 0x100, _fmt, args);
 
     va_end(args);
 }
