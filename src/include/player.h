c43f9717b14d3605d339e48d172865499b290284
maksasj <radioboos@gmail.com>
2022-12-02 17:05:33 +0200

    poggers

--- src/include/player.h
+++ src/include/player.h
@@ -1,13 +1,18 @@
 #ifndef _PLAYER_H_
 #define _PLAYER_H_
 
+#include "buttons.h"
 #include "sprite.h"
 #include "types.h"
 #include "entity/entity.h"
+#include "world.h"
 
 typedef struct PlayerSpecData {
     Sprite* armor;
     Sprite* weapon;
+
+    Sprite* sprites;
+    i32* next_sprite_index;
 } PlayerSpecData;
 
 typedef struct PlayerUI {
@@ -17,8 +22,10 @@ typedef struct PlayerUI {
 
 void initPlayerUI(PlayerUI* _playerUI, Sprite* _sprites, i32* _next_sprite_index);
 
-void initPlayerSpec(Sprite _sprites[], i32* _next_sprite_index, Entity* _entity, PlayerSpecData* _pspec);
+void initPlayerSpec(Sprite* _sprites, i32* _next_sprite_index, Entity* _entity, PlayerSpecData* _pspec);
 
 void updatePlayerSpec(PlayerSpecData* _pspec, Entity *_entity);
+
+void player_update(Entity* _self, World* _world, Room* _room);
 
 #endif
