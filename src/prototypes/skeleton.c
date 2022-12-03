36356b18b069e036cc5935fdccd6b79da1d9fa79
Soskar1 <89640069+Soskar1@users.noreply.github.com>
2022-12-03 20:53:58 +0200

    removed unnecessary comments from skeleton.c

--- src/prototypes/skeleton.c
+++ src/prototypes/skeleton.c
@@ -11,22 +11,18 @@ void skeleton_update(Entity* _self, World* _world, Room* _room) {
     switch (direction)
     {
         case 'u': //Up
-            //_self->position.y += 1;
             _self->vel.y += 0.5;
             _self->facing = UP;
             break;
         case 'r': //Right
-            //_self->position.x += 1;
             _self->vel.x += 0.5;
             _self->facing = RIGHT;
             break;
         case 'd': //Down
-            //_self->position.y -= 1;
             _self->vel.y -= 0.5;
             _self->facing = DOWN;
             break;
         case 'l': //Left
-            //_self->position.x -= 1;
             _self->vel.x -= 0.5;
             _self->facing = LEFT;
             break;
@@ -36,9 +32,6 @@ void skeleton_update(Entity* _self, World* _world, Room* _room) {
 
     _self->position.x += _self->vel.x;
     _self->position.y += _self->vel.y;
-
-    // _self->vel.x = 0;
-    // _self->vel.y = 0;
 }
 
 void skeleton_kill(Entity* _self) {
