ad70f87dd45e1645d7db461e775b170eb5b45b02
maksasj <radioboos@gmail.com>
2023-01-07 23:50:55 +0200

    Rotation

--- include/entity/rotation.h
+++ include/entity/rotation.h
@@ -1,6 +1,17 @@
+/**
+ * Rotation
+ *
+ * @authors
+ * Maksimas Jaroslavcevas, Oskaras Vištorskis
+ *
+ * @brief
+ * File that cotains defenition for all possible rotations
+*/
+
 #ifndef _ROTATION_H_
 #define _ROTATION_H_
 
+/* Enum that defenies all rotations */
 typedef enum Facing {
     UP,
     RIGHT,
@@ -8,6 +19,12 @@ typedef enum Facing {
     LEFT
 } Facing;
 
+/**
+ * Returns opposite rotations
+ * 
+ * @param facing Facing side
+ * @return Opposite facing side
+*/
 Facing getOppositeFacing(Facing facing);
 
 #endif
