fb6d3852a9d2092f5ad98433ca9328647fe70e4d
maksasj <radioboos@gmail.com>
2023-01-21 16:39:06 +0200

    updated tile map

--- src/background.c
+++ src/background.c
@@ -22,15 +22,13 @@ void initBackground(u16* _pallete, u16* _data, i32 _width, i32 _height) {
         (1 << 13) |                 // wrapping flag
         (0 << 14);                  // bg size, 0 is 256x256
     
-    /*
-    *_BG2_CONTROL_ = 1 |            // priority, 0 is highest, 3 is lowest
+    *_BG2_CONTROL_ = 0 |            // priority, 0 is highest, 3 is lowest
         (1 << 2)  |                 // the char block the image data is stored in
         (0 << 6)  |                 // the mosaic flag
         (1 << 7)  |                 // color mode, 0 is 16 colors, 1 is 256 colors
         (3 << 8) |                  // the screen block the tile data is stored in
         (1 << 13) |                 // wrapping flag
         (0 << 14);                  // bg size, 0 is 256x256
-    */
    
     /* Game ui background */
     *_BG3_CONTROL_ = 0 |            // priority, 0 is highest, 3 is lowest
